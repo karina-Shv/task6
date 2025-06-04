c5b7ce3c85a003994f468e074ce2b2f3bca63ba4
karina-Shv <shvalia.karyna@lll.kpi.ua>
2025-06-04 20:28:43 +0300

    -

--- 1-timer.js
+++ 1-timer.js
@@ -1,4 +1,3 @@
-
 const startBtn = document.querySelector("[data-start]");
 const dateTimePicker = document.querySelector("#datetime-picker");
 const daysSpan = document.querySelector("[data-days]");
